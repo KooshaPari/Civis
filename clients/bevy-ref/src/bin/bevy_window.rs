8c0b0fae86c42382d5eb60a7d1ee0f9fda38bff5
KooshaPari <koosha@example.com>
2026-07-21 15:49:08 -0700

    fix(native): isolate local simulation from server attach

--- clients/bevy-ref/src/bin/bevy_window.rs
+++ clients/bevy-ref/src/bin/bevy_window.rs
@@ -250,7 +250,7 @@ fn main() {
     #[cfg(feature = "egui")]
     app.init_state::<AppState>();
     app.init_resource::<LiveStreamScene>()
-        .init_resource::<civ_bevy_ref::AttachMode>()
+        .insert_resource(civ_bevy_ref::resolve_attach_mode_from_env())
         .init_resource::<LiveSceneFocus>()
         .init_resource::<MinimapPopup>()
         .init_resource::<SimSpeedState>()

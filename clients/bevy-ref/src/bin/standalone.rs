76db77babeee05c1dd0d4b48d7c2b6b1cb3522fa
KooshaPari <kooshapari@gmail.com>
2026-08-27 02:58:21 -0700

    feat(client): wire VfxPlugin + CivisAudioPlugin into standalone binary

--- clients/bevy-ref/src/bin/standalone.rs
+++ clients/bevy-ref/src/bin/standalone.rs
@@ -90,6 +90,8 @@ fn main() {
         .add_plugins(civ_bevy_ref::window_icon::WindowIconPlugin)
         .add_plugins(civ_bevy_ref::sim_bridge::SimBridgePlugin)
         .add_plugins(civ_bevy_ref::post_fx::PostFxPlugin)
+        .add_plugins(civ_bevy_ref::vfx::VfxPlugin)
+        .add_plugins(civ_bevy_ref::audio::CivisAudioPlugin)
         .add_plugins(civ_bevy_ref::game_ui::GameUiPlugin)
         .add_plugins(civ_bevy_ref::emergence_dashboard::EmergenceDashboardPlugin)
         .add_plugins(civ_bevy_ref::tech_tree_ui::TechTreeUiPlugin)
